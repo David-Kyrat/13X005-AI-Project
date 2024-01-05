4162dad6cd7227a7d582a4b5b64892dc77439c15
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2024-01-05 09:49:39 +0100

    Gradient descent modified

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,24 +1,27 @@
-def gradient_descent(X: np.ndarray, y: np.ndarray, MyVector: np.ndarray, num_iters: int, alpha: float):
+import numpy as np
+
+def gradient_function(x: float):
+    return np.cos(np.pi * (x + 1)) - x * np.pi * np.sin(np.pi * (x + 1))
+
+
+def gradient_descent(gradient, MyVector, alpha: float, num_iters: int):
     """
     This function implements the gradient descent. It compute the optimal parameters MyVector.
 
     Parameters
     ----------
-    X: np.ndarray
-        Matrix of covariables
-    y: np.ndarray
-        Vector of labels
-    MyVector: np.ndarray
+    gradient:
+        derivate function
+    MyVector:
         Initial vector of parameters to optimize
-    num_iters: int
-        Number of iterations
     alpha: float
         define how the function will converge. Too big values give bad results and too smal values not converge or converge too slowly
-
+    num_iters: int
+        Number of iterations
     Return value
     ------------
     Optimal vector for the initial configuration and parameters
     """
     for i in range(num_iters):
-        MyVector -= alpha * gradient(X, y, MyVector)
+        MyVector -= alpha * gradient(MyVector)
     return MyVector
--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
+from gradient_descent import *
+
 if __name__ == "__main__":
     print("Hello World!")
+
+    test_gradient_descent(function, gradient_function, 0, 0.001, 1000)
