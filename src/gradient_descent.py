087b23769c0f5a8535cfd6d692e956cde2d76724
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2024-01-05 09:56:56 +0100

    Gradient descent finished

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,4 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def function(x: float):
+    return x * np.cos(np.pi * (x + 1))
 
 def gradient_function(x: float):
     return np.cos(np.pi * (x + 1)) - x * np.pi * np.sin(np.pi * (x + 1))
@@ -25,3 +29,15 @@ def gradient_descent(gradient, MyVector, alpha: float, num_iters: int):
     for i in range(num_iters):
         MyVector -= alpha * gradient(MyVector)
     return MyVector
+
+
+def test_gradient_descent(f, df, MyVector, alpha: float, num_iters: int):
+    """
+    This function test the gradient descent implemented
+    """
+    x = np.linspace(0, 1, 100)
+    plt.figure()
+    plt.plot(x, f(x))
+    optimal_x = gradient_descent(df, MyVector, alpha, num_iters)
+    plt.plot(optimal_x, f(optimal_x), "ro")
+    plt.show()
