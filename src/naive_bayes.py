0984a72f840549fb694ffe4b3a0028bc19fb1d92
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-01-08 00:43:00 +0100

    tested predict_naive_bayes

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -96,8 +96,8 @@ def test_predict_bayes():
     params_by_class = get_distrib_parameters(FEAT, COL_NAMES, LABELS)
     # test sample
     idx = np.random.randint(0, len(FEAT))
-    x = FEAT.iloc[idx].to_numpy()
-    print("Sample to predict: ", x)
+    x = FEAT.iloc[idx]
+    print("Sample to predict:\n", x, "\n ")
     pred = predict_bayes(x, params_by_class)
     print("Predicted class: ", pred)
     print("Actual class: ", LABELS.iloc[idx])
