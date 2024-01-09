ddd72d24c7b502a09cf2453317181d6812878f63
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-01-09 16:32:33 +0100

    feat: fixed  test for logistic regression -- >0.9 f1-score

--- src/log_reg.py
+++ src/log_reg.py
@@ -1,3 +1,4 @@
+from _pytest.config.argparsing import OptionGroup
 import numpy as np
 import pytest
 
@@ -5,7 +6,7 @@ import pytest
 from gradient_descent import grad_desc_ml
 
 # NB: floating is any (numpy) floating type NDArray or not
-from numpy import floating as fl
+from numpy import floating as fl, int32 as i32
 from numpy.random import rand, randint
 from numpy.typing import NDArray
 
@@ -59,7 +60,6 @@ def grad(X: NDArray, y: NDArray, w: NDArray, b: float) -> tuple:
 
     X_sum_over_rows = np.sum(X, axis=1)  # Sum over rows of X
     dw = -np.sum(X_sum_over_rows * errors)  # Vectorized computation of dw component
-
     return dw, db
 
 
@@ -82,23 +82,48 @@ def train_log_reg(X: NDArray, y: NDArray, w: NDArray, b: float, n_it: int, lr: f
     Returns
     -------
         Trained (weight vector, bias) with gradient descent that minimize the log loss function."""
-    for _ in range(n_it):
-        grad_w, grad_b = grad(X, y, w, b)
-        w -= lr * grad_w
-        b -= lr * grad_b
-    return w, b
+    if type(X) != np.ndarray: X = np.asarray(X)
+    if type(y) != np.ndarray: y = np.asarray(y)
+    return grad_desc_ml(X, y, grad, w, b, lr, n_it)
 
-    # return grad_desc_ml(X, y, grad, w, b, lr, n_it)
+
+def predict_log_reg(X: NDArray, w: NDArray, b):
+    """Predict the class labels for a set of examples X using logistic regression parameters w and b.
+    :param X: The input features. 2D Matrix NDArray
+    :param w: The weights of the logistic regression model. Vector NDArray
+    :param b: The bias of the logistic regression model. float
+    :return: Vector of predicted class labels (0 or 1) for each example in X. Vector NDArray
+    """
+    # return i32(sigmoid(z(w, X, b)).get() >= 0.5)
+    # print(sigmoid(z(w, X, b)))
+    # predicted= sigmoid((z(w, X, b))).get()
+    if type(X) != np.ndarray: X= np.asarray(X)
+    predicted = sigmoid(norm(z(w, X, b)))
+    class_nb = len(LAB_VAL_IDX)
+    return i32(predicted * class_nb)  # returns k-1 if val < (k/class_nb) i.e. 0 if < 1/3, 1 if < 2/3...
+
+def predict_compute_metrics(X_test, Y_test, w, b):
+    # if DataFrame or Series is passed, convert to numpy array
+    if type(X_test) != np.ndarray: X_test = np.asarray(X_test)
+    predicted_val_logreg = predict_log_reg(X_test, w, b)
+    # metrics = compute_metrics(Y_test, predicted_val_logreg)
+    # return metrics
+
+# ================================================================
+# ======================= TEST:==================================
+# ================================================================
 
 
 @pytest.mark.parametrize("m, n", randint(0, 70, size=(20, 2)))
-def test_log_reg_with_random_values(m, n):
+def test_log_reg_runs_with_random_values(m, n):
     X, y, w, b = rand(m, n), rand(m), rand(n), rand()
     n_it, lr = 100, 0.03
     w, b = train_log_reg(X, y, w, b, n_it, lr)
 
-def test_log_reg_with_dataset_values():
+
+def test_log_reg_runs_with_dataset_values():
     from main import FEAT, LABELS
+
     m, n = FEAT.shape
     init_w = np.random.rand(n)
     init_b = np.random.rand()
@@ -107,3 +132,18 @@ def test_log_reg_with_dataset_values():
     w, b = train_log_reg(FEAT.to_numpy(), LABELS, init_w, init_b, n_it, lr)
     print(w, b)
     print("Found weights and bias:\n\tw =", w, "\tb =", b)
+
+
+def test_log_reg_efficiency():
+    """Test the efficiency of logistic regression with optimally chosen (before) parameters"""
+    from main import FEAT, LABELS, FEAT_test, LABELS_test
+    # NOTE: parameters found after testing with different values, see branch "gpu-training" for more details
+    w, b = np.array([0.53452349, 0.36463584, 1.16132476, 1.08204578]), 0.45146791
+    # pred_compute(FEAT_test, LABELS_test, w, b)
+
+    predicted_val_logreg = predict_log_reg(FEAT_test.to_numpy(), w, b)
+    # TODO: replace with our impl of f1_score
+    from sklearn.metrics import f1_score
+    score = f1_score(LABELS_test, predicted_val_logreg, average="micro")
+    print("weights:", w, "\nbias:", b, "\n")
+    print("F1 score:", score, "\n")
