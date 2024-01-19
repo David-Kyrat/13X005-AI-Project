6cec99a96328d25c186681c3464d1b75f1773f5d
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2024-01-19 12:30:06 +0100

    Softmax finished

--- src/softmax.py
+++ src/softmax.py
@@ -1,61 +1,56 @@
 import numpy as np
 import metrics
 
-# def softmax(X: np.ndarray, theta: np.ndarray) -> np.ndarray:
-# 
-#     if not isinstance(X, np.ndarray):
-#         X = np.asarray(X)
-#     if not isinstance(theta, np.ndarray):
-#         theta = np.asarray(theta)
-#     if len(X.shape) != 2:
-#         print("X must be 2d array")
-#         print("I suppose that X is 1d dimension, so I convert it into 2d dimension")
-#         X = X.reshape(1, -1)
-#     if X.shape[1] == theta.shape[0]:
-#         X_hat = X
-#     else:
-#         X_hat = np.append(X, np.ones((X.shape[0], 1)), axis=1)
-# 
-#     K = np.sum(np.exp(X_hat @ theta), axis=1)
-# 
-#     K = 1/K
-# 
-#     return K[:, None]*np.exp(X_hat @ theta) 
+def softmax(z: np.ndarray) -> np.ndarray:
+    """
+    This function is an implementation of the softmax function
 
-def softmax(x: np.ndarray) -> np.ndarray:
-    assert len(x.shape) == 2
-    norm = x - np.max(x, axis = 1).reshape(-1, 1)
+    Parameters
+    ----------
+    z: np.ndarray
+        Vector or value to compute softmax
+
+    Return value
+    ------------
+        Return the result of the softmax function
+    """
+    assert len(z.shape) == 2, "x must be 2d"
+    # Usefull to avoid overflow. Don't impact the result, because it's like if we have multiply the result by e^-max/e^-max, so by 1...
+    norm = z - np.max(z, axis = 1).reshape(-1, 1)
     return np.exp(norm)/np.sum(np.exp(norm), axis = 1).reshape(-1, 1)
 
 
-theta = np.array([
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [3, 2, 4, 7]])
-
-X = np.array([[0.5, 0.7, 0.8], [0.4, 0.6, 0.3], [0.5, 0.9, 0.2]])
-Y = [0, 2, 1]
-
-#print(softmax(X, theta.T))
-#print(softmax([0.5, 0.7, 0.8], theta.T))
-
 def gradient(X: np.ndarray, theta: np.ndarray, Y: np.ndarray):
+    """
+    This function is the gradient of the negative log likelihood of softmax
+
+    Parameters
+    ----------
+    X: np.ndarray
+        Matrix of known datas
+    theta: np.ndarray
+        Weights and bias
+    Y: np.ndarray
+        Array of known labels
+    """
     if not isinstance(X, np.ndarray):
         X = np.asarray(X)
     if not isinstance(theta, np.ndarray):
         theta = np.asarray(theta)
     if not isinstance(Y, np.ndarray):
         Y = np.asarray(Y)
-    if len(X.shape) != 2:
-        print("X must be 2d array")
-        print("I suppose that X is 1d dimension, so I convert it into 2d dimension")
-        X = X.reshape(1, -1)
-    if X.shape[1] == theta.shape[0]:
+    assert len(X.shape) == 2, "X must be 2d !"
+
+    # Creation of X_hat with a column of ones added
+    if X.shape[1] == theta.shape[1]:
         X_hat = X
     else:
         X_hat = np.append(X, np.ones((X.shape[0], 1)), axis=1)
 
     def f(labels: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        This function compute a matrix witch is equal to 1 if y[i] = column of line i of the matrix
+        """
         if not isinstance(labels, np.ndarray):
             labels = np.asarray(labels)
         result = np.zeros((len(y), len(labels)))
@@ -63,54 +58,83 @@ def gradient(X: np.ndarray, theta: np.ndarray, Y: np.ndarray):
             result[i, y[i]] = 1
         return result
 
-    labels = np.arange(theta.shape[1])
+    labels = np.arange(theta.shape[0])
     gradient_result = np.zeros((theta.shape))
-    gradient_result = gradient_result.T
-        # V1
-        #for i in range(theta.shape[0]):
-        #    gradient_result[i] += X_hat[n] * (f(Y[n], i) - softmax(X_hat[n], theta, i)) 
-        # V2
-    tmp = f(labels, Y) - softmax(X_hat @ theta)
-    for i in range(theta.shape[1]):
-        gradient_result[i] = np.sum(tmp[:, i].reshape(-1, 1) * X_hat, axis = 0)
 
+    tmp = f(labels, Y) - softmax(X_hat @ theta.T)
+    for i in range(theta.shape[0]):
+        gradient_result[i] = np.sum(tmp[:, i].reshape(-1, 1) * X_hat, axis = 0)
+    gradient_result = -gradient_result
     return gradient_result
 
-# print(gradient(X, theta.T, Y))
-
-def gradient_descent(df, alpha: float, num_iters: int, X: np.ndarray, Y: np.ndarray, theta: np.ndarray) -> np.ndarray:
+def gradient_descent(df, X: np.ndarray, Y: np.ndarray, theta: np.ndarray, alpha: float, num_iters: int) -> np.ndarray:
     """This function implements the gradient descent. It iteratively computes the optimal parameters that minimize the given function.
 
     Parameters
     ----------
     `df`: function
         derivative function (i.e. gradient)
+    `X`: np.ndarray
+        Matrix of known parameters
+    `Y`: np.ndarray
+        Vector of known labels
+    `theta`: np.ndarray
+        Parameters to minimise
     `alpha`: float
         define how the function will converge. Values too big will give bad results and values too small won't converge or converge will too slowly
     `num_iters`: int
         Number of iterations
-    `*arg`:
-        All the argument passed to the gradient function
 
     Return value
     ------------
-    Optimal vector for the initial configuration and parameters"""
+    Optimal parameters for the initial configuration and parameters"""
     for _ in range(num_iters):
-        theta -= alpha * df(X, theta.T, Y)
+        theta -= alpha * df(X, theta, Y)
     return theta
 
 def train_log_reg_2(X: np.ndarray, Y: np.ndarray, theta: np.ndarray, num_iter: int, learning_rate: float) -> np.ndarray:
+    """
+    Function to train parameters of a logistic multinomial regression
+
+    Parameters
+    ----------
+    X: np.ndarray
+        Matrix of known parameters
+    Y: np.ndarray
+        Vector of known labels
+    theta: np.ndarray
+        Matrix of initial weight and bias to optimize
+    num_iters: int
+        Number of iterations
+    learning_rate: float
+        Speed of convergence of the gradient descent
+
+    Return value
+    ------------
+    The optimal weights and bias for the datas and initial parameters
+    """
     if not isinstance(X, np.ndarray):
         X = np.asarray(X)
     if not isinstance(Y, np.ndarray):
         Y = np.asarray(Y)
 
-
-    return gradient_descent(gradient, learning_rate, num_iter, X, Y, theta)
-
-# print(train_log_reg_2(X, Y))
+    return gradient_descent(gradient, X, Y, theta, learning_rate, num_iter)
 
 def predict_log_reg_2(X: np.ndarray, theta: np.ndarray) -> np.ndarray:
+    """
+    Function to predict values thanks to logistic multinomial regression
+
+    Parameters
+    ----------
+    X: np.ndarray
+        Matrix of known parameters
+    theta: np.ndarray
+        Matrix of weights and bias
+
+    Return value
+    ------------
+    Return the predicted labels
+    """
     if not isinstance(X, np.ndarray):
         X = np.asarray(X)
     if not isinstance(theta, np.ndarray):
@@ -121,53 +145,50 @@ def predict_log_reg_2(X: np.ndarray, theta: np.ndarray) -> np.ndarray:
         X_hat = np.append(X, np.ones((X.shape[0], 1)), axis=1)
 
     results = softmax(X_hat @ theta.T)
-    print(results)
     prediction = results == np.max(results, axis = 1).reshape(-1, 1)
-    print(prediction)
     return np.where(prediction)[1]
 
-# print(predict_log_reg_2(X, train_log_reg_2(X, Y)))
+def predict_compute_metrics(X_test, Y_test, theta):
+    """
+    Function to compute metrics for a set of data and weights and bias given
 
-def predict_compute_metrics(X_test, Y_test, w, b):
+    Parameters
+    ----------
+    X_test: np.ndarray
+        Set of datas to predict labels
+    Y_test: np.ndarray
+        Real prediction of the labels
+    theta: np.ndarray
+        Weights and bias
+
+    Return value
+    ------------
+    Return the metrics computed
+    """
     if not isinstance(X_test, np.ndarray):
         X_test = np.asarray(X_test)
-    predicted_val_logreg = predict_log_reg(X_test, w, b)
+    predicted_val_logreg = predict_log_reg_2(X_test, theta)
     return metrics.compute_metrics(Y_test, predicted_val_logreg)
 
 def test_log_reg_with_dataset_values():
+    """
+    Function to test logistic multinomial regression with dataset values
+    """
     from main import FEAT, LABELS
 
     X = np.asarray(FEAT)
     _, n = X.shape
-    theta = np.random.randint(1,10, size=(np.unique(LABELS).shape[0], X.shape[1] + 1)).astype(float)
-    theta = np.random.random( size=(np.unique(LABELS).shape[0], X.shape[1] + 1)).astype(float)
-    n_it, lr = 1000, 1e-5
+    # I see that the performances are better for a theta initialised to 0 whereas a random theta...
+    #theta = np.random.randint(1,10, size=(np.unique(LABELS).shape[0], X.shape[1] + 1)).astype(float)
+    theta = np.zeros((np.unique(LABELS).shape[0], X.shape[1] + 1))
+    n_it, lr = 1000, 1e-4
 
     theta = train_log_reg_2(X, LABELS, theta, n_it, lr)
     print("\n\nFound weights and bias:")
     print(theta)
     print()
-    return theta
 
 
-from main import FEAT, LABELS, FEAT_test, LABELS_test
-theta = test_log_reg_with_dataset_values()
-print(predict_log_reg_2(FEAT_test, theta))
-exit()
-X = np.asarray(FEAT)
-learning_rate = np.linspace(0.1, 1e-5, 10)
-index = 0
-maximum = 0
-for i in range(len(learning_rate)):
-    theta = np.random.randint(1,10, size=(np.unique(LABELS).shape[0], X.shape[1] + 1)).astype(float)
-    theta = train_log_reg_2(X, LABELS, theta, 1000, learning_rate[i])
-    predicted_val_logreg = predict_log_reg_2(np.asarray(FEAT_test), theta)
-    scores = metrics.compute_metrics(LABELS_test, predicted_val_logreg)
-    if scores['f1_score'] > maximum:
-        maximum = scores['f1_score']
-        index = i
-print(maximum)
-print(index)
 
 def test_log_reg_f1score():
     """Test the efficiency of logistic regression with optimally chosen (before) parameters"""
@@ -175,17 +196,11 @@ def test_log_reg_f1score():
 
     X = np.asarray(FEAT)
     _, n = X.shape
-    index = 0
-    maximum = 0
-    n_it = 1000
-    lr = 1e-5
-    for i in range(5):
-        theta = train_log_reg_2(X, LABELS, theta, n_it, lr)
-        predicted_val_logreg = predict_log_reg_2(np.asarray(FEAT_test), theta)
-        scores = metrics.compute_metrics(LABELS_test, predicted_val_logreg)
-        if scores['f1_score'] > maximum:
-            index = i
-        lr *= 10
-    print(maximum)
+    theta = np.zeros((np.unique(LABELS).shape[0], X.shape[1] + 1))
+    n_it, lr = 1000, 1e-4
+    theta = train_log_reg_2(X, LABELS, theta, n_it, lr)
+    print(predict_compute_metrics(FEAT_test, LABELS_test, theta))
 
-
+if __name__ == "__main__":
+    test_log_reg_with_dataset_values()
+    test_log_reg_f1score()
